be2a88a10ce5070f7621b6da2d96a64ec4272e0f
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-19 17:28:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,7 +24,7 @@ class CashRegister
       "There is no discount to apply."
     else
       @total -= (@total*(@discount/100.0))
-      puts "After the discount, the total comes to #{employee_discount}."
+      puts "After the discount, the total comes to #{@total}."
     end
   end
 
