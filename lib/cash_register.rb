8d68d19a8954082bacb268a7f2eaf78d6be9d571
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-19 17:49:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ class CashRegister
     if @discount == 0
       "There is no discount to apply."
     else
-      @discount = @discount/95.to_f
+      @discount = @discount/94.to_f
       @total = (@total - (@total * @discount)).to_i
       "After the discount, the total comes to $#{@total}."
     end
